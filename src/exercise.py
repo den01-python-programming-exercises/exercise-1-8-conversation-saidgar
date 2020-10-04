f370fb1cc8857687f2898fb865283c1330f61f25
saidgar <sebast45ian@gmail.com>
2020-10-04 12:31:12 +0200

    Attempt 1.

--- src/exercise.py
+++ src/exercise.py
@@ -1,8 +1,10 @@
 def main():
     #write your code below this line
     first = input("Greetings! How are you doing?")
+    print(first)
     second = input("Oh, how interesting. Tell me more!")
+    print(second)
     third = input("Thanks for sharing!")
-
+    print(third )
 if __name__ == '__main__':
     main()
